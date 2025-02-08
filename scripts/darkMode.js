2e6d0c97c1a5b6c5a9529bad5933e80926689b64
nicolas-droppa <droppa.nicolas@gmail.com>
2025-02-08 13:15:53 +0100

    dark-mode implementation

---
+++ scripts/darkMode.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('dayNightButton');
+
+function applyDarkMode() {
+    const darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'enabled') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+applyDarkMode();
+
+button.addEventListener('click', () => {
+    const darkMode = localStorage.getItem('dark-mode');
+
+    if (darkMode === 'enabled') {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('dark-mode', 'disabled');
+    } else {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+});
