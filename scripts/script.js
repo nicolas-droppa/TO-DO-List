cbf9deb4db21b8f593f2547de7943d1ad0dd071c
nicolas-droppa <droppa.nicolas@gmail.com>
2025-02-08 12:18:49 +0100

    defualt todolist and funnction setup

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const buttonAddTask = document.getElementById("addTaskButton");
+
+buttonAddTask.addEventListener("click", function () {
+    addTask();
+});
+
+function createList(){
+
+}
+
+function deleteList(){
+
+}
+
+function addTask(){
+    
+}
+
+function deleteTask(){
+
+}
