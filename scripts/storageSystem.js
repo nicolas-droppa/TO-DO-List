f971b4f67e1b3223bb3b52a0c49754733769825e
nicolas-droppa <droppa.nicolas@gmail.com>
2025-02-21 23:57:16 +0100

    function preparation

---
+++ scripts/storageSystem.js
@@ -0,0 +1,19 @@
+/**
+ * Saves data to local storage
+ * @param 
+ */
+export function saveToLocalStorage(data) {
+
+}
+
+/**
+ * Loads data from local storage
+ * @returns 
+ */
+export function loadFromLocalStorage() {
+
+}
+
+export function updateData() {
+    
+}
